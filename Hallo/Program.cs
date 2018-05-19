34affcdb37ff733fe1e75714d977d22f826277ef
Krystian Pawlik <krystian.pawlik.1985@outlook.com>
2018-05-19 06:40:22 +0200

    Compilled

--- Hallo/Program.cs
+++ Hallo/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
 namespace Hallo
 {
@@ -10,6 +7,12 @@ namespace Hallo
     {
         static void Main(string[] args)
         {
+            string name = args[0];
+            Console.WriteLine("Witaj, " + name);
+            //foreach (var item in args)
+            //{
+            //    Console.WriteLine("Witaj, " + item);
+            //}
         }
     }
 }
